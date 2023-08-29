2b5b3b69bfb95dadd9b0faceee2e1aba3c3392b8
Nuar <hajime_jp@yahoo.com>
2023-08-29 14:03:30 +0900

    First commit

---
+++ Source/House_Test/House_TestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "House_TestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOUSE_TEST_API AHouse_TestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
